3dc718f2a962bce435c2e8230107f65647a66727
NAoHR <najmim625@gmail.com>
2022-03-06 14:56:46 +0700

    mobile size bug

--- components/Balls.jsx
+++
@@ -1,7 +0,0 @@
-import style from "../styles/components-css/Balls.module.css"
-
-const Balls = () => {
-    return <div className={style.balls}></div>
-}
-
-export default Balls;
--- containers/Main.jsx
+++ containers/Main.jsx
@@ -1,5 +1,4 @@
 import Navbar from "../components/Navbar";
-import Balls from "../components/Balls";
 import style from "../styles/containers-css/Main.module.css";
 import Link from "next/link";
 
@@ -7,29 +6,33 @@ const Main = () => {
     return (
         <>
         <Navbar />
-        <div className={style.main_wrapper}>
-            <Balls />
-            <div className={style.main_content_opener}>
-                    <div className={style.left_side}>
-                        <h1 className={style.main_text}>
-                            <span className={style.orange}>sija</span> smkn negeri 26 <br/> Jakarta
-                        </h1>
-                        <p className={style.p_text}>Belajar, Bekerja, membangun dengan semangat yang penuh dan tidak lupa disertai dengan doa dan keihklasan agar bisa mencapai kejayaan di masa depan</p>
-                        <div className={style.explore_wrapper}>
-                            <Link href="https://youtu.be/dQw4w9WgXcQ">
-                                <a>        
-                                    <h1 className={style.explore_text}>explore</h1>
-                                </a>
-                            </Link>
+        <div className={`${style.main_wrapper} ${style.main_padding}`}>
+            <div className={style.main_content}>
+                <div className={style.balls_right}></div>
+                <div className={style.main_content_opener}>
+                        <div className={style.left_side}>
+                            <h1 className={style.main_text}>
+                                <span className={style.orange}>sija</span> smkn negeri 26 <br/> Jakarta
+                            </h1>
+                            <p className={style.p_text}>Belajar, Bekerja, membangun dengan semangat yang penuh dan tidak lupa disertai dengan doa dan keihklasan agar bisa mencapai kejayaan di masa depan</p>
+                            <div className={style.explore_wrapper}>
+                                <Link href="https://youtu.be/dQw4w9WgXcQ">
+                                    <a>        
+                                        <h1 className={style.explore_text}>explore</h1>
+                                    </a>
+                                </Link>
+                            </div>
                         </div>
-                    </div>
-                    <div className={style.right_side}>
-                        <img src="/sija_logo.png" alt="logo_sija.png" className={style.image_logo}/>
-                    </div>
+                        <div className={style.right_side}>
+                            <img src="/sija_logo.png" alt="logo_sija.png" className={style.image_logo}/>
+                        </div>
+                </div>
+
+            </div>
+            <div className={style.testing}>
+
             </div>
         </div>
-        {/* <PageWrapper pt="60px" ch="res">
-        </PageWrapper> */}
         </>
     )
 }
