1b69ab71d6f1ed7d1d851499d98ef5db868dc866
NAoHR <najmim625@gmail.com>
2022-03-06 23:09:23 +0700

    testing responsiveness

--- containers/Main.jsx
+++ containers/Main.jsx
@@ -1,38 +1,31 @@
+import style from "../styles/containers-css/Testing.module.css";
 import Navbar from "../components/Navbar";
-import style from "../styles/containers-css/Main.module.css";
 import Link from "next/link";
 
 const Main = () => {
     return (
         <>
-        <Navbar />
-        <div className={`${style.main_wrapper} ${style.main_padding}`}>
-            <div className={style.main_content}>
-                <div className={style.balls_right}></div>
-                <div className={style.main_content_opener}>
-                        <div className={style.left_side}>
-                            <h1 className={style.main_text}>
-                                <span className={style.orange}>sija</span> smkn negeri 26 <br/> Jakarta
-                            </h1>
-                            <p className={style.p_text}>Belajar, Bekerja, membangun dengan semangat yang penuh dan tidak lupa disertai dengan doa dan keihklasan agar bisa mencapai kejayaan di masa depan</p>
-                            <div className={style.explore_wrapper}>
-                                <Link href="https://youtu.be/dQw4w9WgXcQ">
-                                    <a>        
-                                        <h1 className={style.explore_text}>explore</h1>
-                                    </a>
-                                </Link>
-                            </div>
+            <Navbar />
+            <div className={style.lovely_welcoming}>
+                <div className={style.lovely_divider}>
+                    <div className={style.lovely_left}>
+                        <h1 className={style.lovely_main_text}>
+                            <span className={style.lovely_orange}>sija</span> smkn negeri 26 <br/> Jakarta
+                        </h1>
+                        <p className={style.lovely_p_text}>Belajar, Bekerja, membangun dengan semangat yang penuh dan tidak lupa disertai dengan doa dan keihklasan agar bisa mencapai kejayaan di masa depan</p>
+                        <div className={style.lovely_explore_wrapper}>
+                            <Link href="https://youtu.be/dQw4w9WgXcQ">
+                                <a>        
+                                    <h1 className={style.lovely_explore_text}>explore</h1>
+                                </a>
+                            </Link>
                         </div>
-                        <div className={style.right_side}>
-                            <img src="/sija_logo.png" alt="logo_sija.png" className={style.image_logo}/>
-                        </div>
+                    </div>
+                    <div className={style.lovely_right}>
+                        <img src="/sija_logo.png" alt="logo_sija.png" className={style.lovely_image_logo}/>
+                    </div>
                 </div>
-
             </div>
-            <div className={style.testing}>
-
-            </div>
-        </div>
         </>
     )
 }
