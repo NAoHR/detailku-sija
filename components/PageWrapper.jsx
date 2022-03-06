4e2380b976e6d1047124f07683e4a784380a3a90
NAoHR <najmim625@gmail.com>
2022-03-06 10:06:54 +0700

    fix responsive issue

--- components/PageWrapper.jsx
+++ components/PageWrapper.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import style from "../styles/components-css/PageWrapper.module.css";
 
 const PageWrapper = (props) => {
+    const [height,setHeight] = useState(false);
+    const {pt, ch} = props
+    useEffect(()=>{
+        window.addEventListener("resize",() => {
+            if(window.innerHeight >= 1024 && ch === "res"){
+                setHeight(true);
+            }else{
+                setHeight(false)
+            }
+        })
+    },[]);
+
     return (
         <>
         <div className={style.balls}></div>
-        <div className={style.main_full}>
+        <div className={style.main_full} style={{
+            "paddingTop" : pt ? pt : "0px",
+            height : height? "1024px" : "100vh"
+        }}>
             {props.children}
         </div>
         </>
