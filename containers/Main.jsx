4b5261527304a97fe512b54c72838a8f112e3ee1
NAoHR <najmim625@gmail.com>
2022-03-06 10:07:21 +0700

    minor change

--- containers/Main.jsx
+++ containers/Main.jsx
@@ -6,13 +6,16 @@ const Main = () => {
     return (
         <>
         <Navbar />
-        <PageWrapper>
+        <PageWrapper pt="60px" ch="res">
             <div className={style.main_content_opener}>
                     <div className={style.left_side}>
                         <h1 className={style.main_text}>
                             <span className={style.orange}>smk</span> negeri 26 <br/> Jakarta
                         </h1>
                         <p className={style.p_text}>Belajar, Bekerja, membangun dengan semangat yang penuh dan tidak lupa disertai dengan doa dan keihklasan agar bisa mencapai kejayaan di masa depan</p>
+                        <div className={style.explore_wrapper}>
+
+                        </div>
                     </div>
                     <div className={style.right_side}>
                         <img src="/sija_logo.png" alt="logo_sija.png" className={style.image_logo}/>
