f8280397c50d0501c50c00e0690c0747b5ccf887
NAoHR <najmim625@gmail.com>
2022-03-07 14:37:22 +0700

    add who segment

--- containers/Main.jsx
+++ containers/Main.jsx
@@ -1,4 +1,4 @@
-import style from "../styles/containers-css/Testing.module.css";
+import style from "../styles/containers-css/Main.module.css";
 import Navbar from "../components/Navbar";
 import Link from "next/link";
 
@@ -6,14 +6,14 @@ const Main = () => {
     return (
         <>
             <Navbar />
-            <div className={style.lovely_welcoming}>
+            <div className={style.lovely_wrapper}>
                 <div className={style.lovely_divider}>
-                    <div className={style.lovely_left}>
-                        <h1 className={style.lovely_main_text}>
+                    <div className={`${style.lovely_left} ${style.lovely_z_index}`}>
+                        <h1 className={`${style.lovely_main_text} ${style.lovely_z_index}`}>
                             <span className={style.lovely_orange}>sija</span> smkn negeri 26 <br/> Jakarta
                         </h1>
-                        <p className={style.lovely_p_text}>Belajar, Bekerja, membangun dengan semangat yang penuh dan tidak lupa disertai dengan doa dan keihklasan agar bisa mencapai kejayaan di masa depan</p>
-                        <div className={style.lovely_explore_wrapper}>
+                        <p className={`${style.lovely_p_text} ${style.lovely_z_index}`}>Belajar, Bekerja, membangun dengan semangat yang penuh dan tidak lupa disertai dengan doa dan keihklasan agar bisa mencapai kejayaan di masa depan</p>
+                        <div className={`${style.lovely_explore_wrapper} ${style.lovely_z_index}`}>
                             <Link href="https://youtu.be/dQw4w9WgXcQ">
                                 <a>        
                                     <h1 className={style.lovely_explore_text}>explore</h1>
@@ -22,12 +22,52 @@ const Main = () => {
                         </div>
                     </div>
                     <div className={style.lovely_right}>
-                        <img src="/sija_logo.png" alt="logo_sija.png" className={style.lovely_image_logo}/>
+                        <img src="/sija_logo.png" alt="logo_sija.png" className={`${style.lovely_image_logo} ${style.lovely_z_index}`}/>
                     </div>
                 </div>
             </div>
-        </>
-    )
+            <div className={`${style.lovely_who_wrapper} ${style.lovely_who}`}>
+                <div className={style.lovely_detail_wrapper}>
+                    <div className={style.lovely_who_ball}>
+                    </div>
+                    <div className={style.lovely_flexed_who}>
+                            <div className={style.lovely_who_text_wrap}>
+                                <div className={style.lovely_head_wrapper}>
+                                    <h1 className={style.lovely_who_head}>
+                                        Who We Are
+                                    </h1>
+                                    <div className={style.lovely_long_bar}>
+
+                                    </div>
+                                </div>
+                                <p className={style.lovely_who_text}>
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo consectetur omnis officiis iste dolore! Unde earum laudantium, nisi ipsum mollitia, accusamus, maiores ad dolores saepe tempora aliquam ex. Maxime, delectus.
+                                </p>
+                            </div>
+                        <div className={style.lovely_detail_who}>
+                            <div className={style.lovely_who_card}>
+                                <div className={style.lovely_card_ball}>
+
+                                </div>
+                            </div>
+                            <div className={style.lovely_who_card}>
+                                <div className={style.lovely_card_ball}>
+                                    
+                                </div>
+                            </div>
+                            <div className={style.lovely_who_card}>
+                                <div className={style.lovely_card_ball}>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={style.absolute_lovely_ball}>
+
+            </div>
+        </>)
 }
 
 export default Main;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Main from "../containers/Main";
+// import Testing from "../containers/Testing";
+
 
 export default function Home() {
   return (
