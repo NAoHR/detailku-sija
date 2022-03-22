b678e1a0c5d966e04aeb7943ea160b70dc6eb456
NAoHR <najmim625@gmail.com>
2022-03-22 22:10:16 +0700

    api essentials

---
+++ utils/apiCaller.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const url = {
+    job : "https://detailku-server.herokuapp.com/api/client/jobs",
+    gradeBased : "https://detailku-server.herokuapp.com/api/client/grade/",
+    usernameBased : "https://detailku-server.herokuapp.com/api/client/user/"
+}
+
+export const requestMethod = {
+    getAllJob : () => {
+        console.log(url.job);
+        return axios.get(url.job)
+    },
+    getGradeBased : ({grade}) => {
+        return axios.get(`${url.gradeBased}${grade}`)
+    },
+    getUsernameBased : ({username}) => {
+        return axios.get(`${url.usernameBased}${username}`)
+    }
+}
