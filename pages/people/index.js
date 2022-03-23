17026de38a91a2009889037206c97a06301f577a
NAoHR <najmim625@gmail.com>
2022-03-23 20:42:59 +0700

    people page

---
+++ pages/people/index.js
@@ -0,0 +1,25 @@
+import People from "../../containers/People.jsx";
+import {requestMethod} from "../../utils/apiCaller";
+import Loader from "../../components/Loader";
+import { useEffect,useState } from "react";
+
+const PeopleMain = () => {
+    const [data,setData] = useState(null)
+    useEffect(()=>{
+        requestMethod.getPeople()
+            .then((val)=>{
+                setData(val.data.data);
+            })
+            .catch((err)=>{
+                setData(false)
+            })
+    },[])
+    return (
+        <>
+            {data !== null && <People data={data}/>}
+            <Loader status={data !== null && true}/>
+        </>
+    )
+}
+
+export default PeopleMain;
