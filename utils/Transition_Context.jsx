45c1eb139942b6ea63af13266378b292755ce2a5
NAoHR <najmim625@gmail.com>
2022-03-26 21:51:28 +0700

    add each page transition

---
+++ utils/Transition_Context.jsx
@@ -0,0 +1,44 @@
+import {useContext,useState,useEffect, createContext} from "react";
+import { useRouter } from "next/router";
+
+
+export const TransitContext = createContext();
+
+const TransitionContext = (props) => {
+    const [pageToGo, setPTG] = useState(null);
+    const [isReady,setIsReady] = useState(false)
+    const router = useRouter();
+    
+    function pageTransitHandler(togo){
+        if(pageToGo === null){
+            setPTG(togo);
+        }
+    }
+
+    useEffect(()=>{
+        if(pageToGo !== null){
+            setTimeout(async ()=>{
+                await router.push(pageToGo);
+                setPTG(null);
+            },1200)
+        }
+    },[pageToGo])
+
+    useEffect(()=>{
+        setIsReady(router.isReady)
+    },[isReady])
+    
+    return (
+    <>
+        <TransitContext.Provider value={pageTransitHandler} >
+        {props.children}
+        <div className="lovely_transit_page" style={{
+            height : pageToGo !== null && "100%",
+            transition : "1s"
+        }}>
+        </div>
+        </TransitContext.Provider>
+    </>)
+}
+
+export default TransitionContext;
