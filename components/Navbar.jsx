1f9859bec4a4a89fa1ab8a1f4f249cb05912e948
NAoHR <najmim625@gmail.com>
2022-06-11 19:08:49 +0700

    refactor: lil touch

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,40 @@
 import style from "../styles/components-css/Navbar.module.css";
 import CustomLink from "../utils/Custom_link";
+import { useRouter } from "next/router";
+import { useState,useEffect } from "react";
 
-import { useState } from "react";
 import {
     FaBars,FaTimes
 } from "react-icons/fa";
 
 export default function Navbar(){
     const [expand, setExpand] = useState(false);
+    const [page,setPage] = useState(null);
+    const router = useRouter();
+    function setColor(page,given){
+        return {
+            "textDecoration" : `${page == given ? "underline var(--orange)" : "none"}`
+        }
+    }
+    useEffect(()=>{
+        let pathName = router.pathname;
+        if(pathName){
+            pathName = pathName.split("/")[1];
+            switch(pathName){
+                case "":
+                    setPage("home")
+                    return;
+                default:
+                    setPage(pathName);
+                    return;
+            }
+        }
+
+    },[router.isReady])
+
+    useEffect(()=>{
+        console.log(page);
+    },[page])
     return (
         <>
         <nav className={style.nav_wrapper}>
@@ -22,28 +49,28 @@ export default function Navbar(){
                 <div className={`${style.center_side}  ${style.align_center}`}>
                     <span className={style.center_span}>
                         <CustomLink path={"/"}>
-                            <h1 className={style.center_side_content}>
+                            <h1 className={style.center_side_content} style={setColor(page,"home")}>
                                 Home
                             </h1>
                         </CustomLink>
                     </span>
                     <span className={style.center_span}>
                         <CustomLink path={"/about"}>
-                            <h1 className={style.center_side_content}>
+                            <h1 className={style.center_side_content} style={setColor(page,"about")}>
                                 About
                             </h1>
                         </CustomLink>
                     </span>
                     <span className={style.center_span}>
                         <CustomLink path={"/people"}>
-                            <h1 className={style.center_side_content}>
+                            <h1 className={style.center_side_content} style={setColor(page,"people")}>
                                 People
                             </h1>
                         </CustomLink>
                     </span>
                     <span className={style.center_span}>
                         <CustomLink path={"/job"}>
-                            <h1 className={style.center_side_content}>
+                            <h1 className={style.center_side_content} style={setColor(page,"job")}>
                                 Job
                             </h1>
                         </CustomLink>
@@ -70,28 +97,28 @@ export default function Navbar(){
                         <div className={style.expander_wrapper}>
                             <div className={style.expander_item}>
                                 <CustomLink path={"/"}>
-                                    <h1 className={style.expander_text} >
+                                    <h1 className={style.expander_text} style={setColor(page,"home")}>
                                         Home
                                     </h1>
                                 </CustomLink>
                             </div>
                             <div className={style.expander_item}>
                                 <CustomLink path={"/people"}>
-                                    <h1 className={style.expander_text} >
+                                    <h1 className={style.expander_text} style={setColor(page,"people")}>
                                         People
                                     </h1>
                                 </CustomLink>
                             </div>
                             <div className={style.expander_item}>
                                 <CustomLink path={"/job"}>
-                                    <h1 className={style.expander_text} >
+                                    <h1 className={style.expander_text} style={setColor(page,"job")}>
                                         Job
                                     </h1>
                                 </CustomLink>
                             </div>
                             <div className={style.expander_item}>
                                 <CustomLink path={"/about"}>
-                                    <h1 className={style.expander_text} >
+                                    <h1 className={style.expander_text} style={setColor(page,"about")}>
                                         About
                                     </h1>
                                 </CustomLink>
