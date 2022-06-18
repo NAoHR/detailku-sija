89c83df1ce6de0dd74f287f8f90ef3177db0bf4f
NAoHR <najmim625@gmail.com>
2022-06-18 10:05:58 +0700

    refactor: remove unnesecary loader

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,9 @@
 import About_Main from "../containers/About";
-import Loader from "../components/Loader";
-import { useState,useEffect } from "react";
 
 const About = () => {
-    const [data,setData] = useState(null);
-    useEffect(()=>{
-      setTimeout(()=>{
-        setData(true)
-      },1000)
-    },[])
     return (
         <>
-        {data !== null && 
-            <About_Main />
-        }
-        <Loader status={data !== null && true}/>
+          <About_Main />
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,9 @@
 import Main from "../containers/Main";
-import Loader from "../components/Loader";
-import { useState,useEffect } from "react";
 
 export default function Home() {
-  const [data,setData] = useState(null);
-  useEffect(()=>{
-    setTimeout(()=>{
-      setData(true)
-    },1000)
-  },[])
   return (
     <>
-
-      {data !== null && 
-        <Main />
-      }
-      <Loader status={data !== null && true}/>
+      <Main />
     </>
   )
 }
