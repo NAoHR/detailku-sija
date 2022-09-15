0b31af6466168260f9f3638a4683fd70ee43625d
naohr <najmim625@gmail.com>
2022-09-15 16:55:50 +0700

    fix: fix production bug

--- components/About/About_map.jsx
+++ components/About/About_map.jsx
@@ -28,8 +28,8 @@ const About_map = () => {
                 <h2 className={style.vt_text} >Sekilas Tentang Kami </h2>
             </div>
             <div className={style.flexit}>
-                <iframe height="450" id="gmap_canvas" src="https://maps.google.com/maps?q=smkn%2026%20jakarta&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" className={`${style.vmap} ${style.brdr}`} ></iframe>
-                <iframe className={`${style.vTour} ${style.brdr}`} src="https://app.lapentor.com/sphere/smkn-26-1602643278?scene=5f87dffda66d425c4c007d27" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe height="450" id="gmap_canvas" src="https://maps.google.com/maps?q=smkn%2026%20jakarta&t=&z=15&ie=UTF8&iwloc=&output=embed" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" className={`${style.vmap} ${style.brdr}`} ></iframe>
+                <iframe className={`${style.vTour} ${style.brdr}`} src="https://app.lapentor.com/sphere/smkn-26-1602643278?scene=5f87dffda66d425c4c007d27" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
 
         </div>
